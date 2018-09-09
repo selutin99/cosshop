338b27685a65bb20f883061007cc052a192edf14
Alexander <cool.selutin99@yandex.ru>
2018-09-09 20:58:44 +0400

    Main

--- index.php
+++ index.php
@@ -2,29 +2,117 @@
 <html>
     <head>
 
-        <title>Креативный интернет-магазин</title>
+        <title>Creative Online Store - Главная</title>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        <link rel="stylesheet" href="css/bootstrap.min.css" >
-
+		
+		<link rel="shortcut icon" type="image/png" href="images/logo/favicon.png"/>
+		
+        <link rel="stylesheet" href="css/bootstrap.min.css">	
+		<link rel="stylesheet" href="css/main.css">
+		
     </head>
     <body>
+		<!--HEADER-->
+		<header>
+			<nav class="navbar navbar-inverse navbar-static-top">
+				<div class="container-fluid">
+					<!--LOGO-->
+					<div class="navbar-header">
+						<a class="navbar-brand" href="#"><img src="images/logo/logo.svg" style="height: 40px; display: inline-block; margin-top: -15px;" alt="COSshop">
+						COSshop
+						</a>
+					</div>
+					<!--END LOGO-->
+				
+					<ul class="nav navbar-nav">
+						<li class="active"><a href="#">Главная</a></li>
+						<li><a href="#">О нас</a></li>
+						<li><a href="#">Контакты</a></li>
+					</ul>
+					
+					<ul class="nav navbar-nav navbar-right">
+						<li><a href="#"><span class="glyphicon glyphicon-user"></span> Регистрация</a></li>
+						<li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Вход</a></li>
+					</ul>
+					
+					<!--SEARCH-->
+					<form class="navbar-form navbar-right" action="/action_page.php">
+						<div class="input-group">
+						<input type="text" class="form-control" placeholder="Поиск">
+							<div class="input-group-btn">
+								<button class="btn btn-default" type="submit">
+									<i class="glyphicon glyphicon-search"></i>
+								</button>
+							</div>
+						</div>
+					</form>
+					<!--END SEARCH-->
+			  </div>
+			</nav>
+		</header>
+		<!-- END HEADER-->
+		
+		<!--SLIDER-->
+		<div class="container" style="padding: 0; width: 100%; margin-top: -20px;">
+			<div id="myCarousel" class="carousel slide" data-ride="carousel">
+				<!--INDICATORS-->
+				<ol class="carousel-indicators">
+				  <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+				  <li data-target="#myCarousel" data-slide-to="1"></li>
+				  <li data-target="#myCarousel" data-slide-to="2"></li>
+				</ol>
+				<!--END INDICATORS-->
+				
+				<div class="container">
+					<h1 style="color: black;">TEST TEXT FOR PHONE NUMBER</h1>
+				</div>
+				
+				<!--SLIDES-->
+				<div class="carousel-inner">
+				  <div class="item active">
+					<img src="images/not.jpg" class="center-block" style="width:60%; max-height: 400px;">
+					<div class="carousel-caption">
+					  <h3>Ноутбуки</h3>
+					  <p>Мощные!</p>
+					</div>
+				  </div>
 
-        <h1>Hello world!</h1>
-        <button type="button" class="btn btn-primary" data-toggle="popover"
-                title="Сообщение" data-content="Ура, Bootstrap 4 работает">
-            Поднеси ко мне курсор
-        </button>
+				  <div class="item">
+					<img src="images/ny.jpeg" class="center-block" style="width:60%; max-height: 400px;">
+					<div class="carousel-caption">
+					  <h3>Мобилки</h3>
+					  <p>Сочные!</p>
+					</div>
+				  </div>
+				
+				  <div class="item">
+					<img src="images/n.png" class="center-block" style="width:60%; max-height: 400px;">
+					<div class="carousel-caption">
+					  <h3>Наушники</h3>
+					  <p>Вкусные</p>
+					</div>
+				  </div>
+			  
+				</div>
+				<!--END SLIDES-->
 
+				<!--CONTROLLERS-->
+				<a class="left carousel-control" href="#myCarousel" data-slide="prev">
+				  <span class="glyphicon glyphicon-chevron-left"></span>
+				  <span class="sr-only">Предыдущий</span>
+				</a>
+				<a class="right carousel-control" href="#myCarousel" data-slide="next">
+				  <span class="glyphicon glyphicon-chevron-right"></span>
+				  <span class="sr-only">Следующий</span>
+				</a>
+				<!--END CONTROLLERS-->
+		    </div>
+		</div>
+		
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
         <script src="js/bootstrap.min.js"></script>
-
-        <script>
-            $(function () {
-                $('[data-toggle="popover"]').popover({trigger:'hover'});
-            });
-        </script>
     </body>
 </html>
