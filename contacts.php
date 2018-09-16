211eced3d4f6ea53ea86108fbb9aded14921e45a
Alexander Selyutin <cool.selutin99@yandex.ru>
2018-09-16 13:13:49 +0400

    Little fix

--- contacts.php
+++ contacts.php
@@ -2,7 +2,7 @@
 <html>
     <head>
 
-        <title>Creative Online Store - Главная</title>
+        <title>Creative Online Store - Контакты</title>
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
